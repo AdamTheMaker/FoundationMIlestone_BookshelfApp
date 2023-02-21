9b5f54d52c6a583d8304e89ab9e405ecad2d1a79
AdamTheMaker <adam.bojoh@gmail.com>
2023-02-21 17:50:12 -0500

    added search and fixed comments to persist after searchs/sorts

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -7,6 +7,7 @@ class Book {
     this.language = language;
     this.subject = subject;
     this.title = title;
+    this.allComments = []
     }
 
     render() {
@@ -31,20 +32,34 @@ class Book {
         //comments - going to try and do a button that triggers a popup where you can input the comment and then it adds below the visualized book on the page
         const commentButton = document.createElement('button');
         commentButton.textContent = 'Comment';
+        
         const commentList = document.createElement('ul');
         commentList.classList.add('comments');
+        
         commentButton.addEventListener('click', function(){
             let comments = prompt(`leave a comment about ${this.title} (only 280 characters will be kept):`, '');
             if (comments == null || comments =="") {
                 window.alert('you have left no comment')
             } else {
                 comments = comments.substring(0, 280);
-                let commentPost = document.createElement('li');
-                commentPost.textContent = comments;
-                commentList.append(commentPost)
+                this.allComments.push(comments);
+                commentList.innerHTML = '';
+                this.allComments.forEach((comment) => {
+                    const commentPost = document.createElement('li');
+                    commentPost.textContent = comment;
+                    commentList.append(commentPost)
+                })
             }
         }.bind(this))
-                 
+        
+        if(this.allComments.length > 0) {
+            this.allComments.forEach((comment) => {
+                const commentPost = document.createElement('li');
+                commentPost.textContent = comment;
+                commentList.append(commentPost)
+            })
+        }
+
         binding.append(bookTitle);
         binding.append(bookAuthor);
         binding.append(infoButton)
@@ -57,10 +72,6 @@ class Book {
     }
 }
 
-
-//code used to test and console out functions to ensure code was working
-// const testBook = new Book(bookData[1].author, bookData[1].language, bookData[1].subject, bookData[1].title)
-// console.log(testBook)
 
 class Bookshelf {
     constructor() {
@@ -77,32 +88,36 @@ class Bookshelf {
             this.shelf.push(builtBook)
         })
     }
-   
+    
     shelfStocker() {
         this.shelf.map((element) => document.querySelector('.books').append(element.render()))
     }
     
     // These features are not needed for the final but may impliment anyway if I decide to
-
+    
     // sortAtoZ() {
-    //     this.shelf.sort((a, b) => a.title.localeCompare(b.title));
-    // }
-
-    // search(query) {
-    //     const matchingBooks = this.shelf.filter((book) => {
-    //         let matchTitle = book.title.toLowerCase().includes(query.toLowerCase());
-    //         let authorString = book.author.join(' ');
-    //         let matchAuthor = authorString.toLowerCase().includes(query.toLowerCase());
-    //         let subjectString = book.subject.join(" ");
-    //         let matchSubject = subjectString.toLowerCase().includes(query.toLowerCase());
-    //         return matchTitle || matchAuthor || matchSubject;
-    //     });
-    //     const bookList = document.querySelector('.books');
-    //     bookList.innerHTML = "";
-    //     matchingBooks.forEach((book) =>{
-    //         const bookElement = book.render();
-    //         bookList.appendChild(bookElement);
-    //     })
-    // }
+        //     this.shelf.sort((a, b) => a.title.localeCompare(b.title));
+        // }
+        
+        search(query) {
+            const matchingBooks = this.shelf.filter((book) => {
+                let matchTitle = book.title.toLowerCase().includes(query.toLowerCase());
+                let authorString = book.author.join(' ');
+                let matchAuthor = authorString.toLowerCase().includes(query.toLowerCase());
+                let subjectString = book.subject.join(" ");
+                let matchSubject = subjectString.toLowerCase().includes(query.toLowerCase());
+                return matchTitle || matchAuthor || matchSubject;
+            });
+            const bookList = document.querySelector('.books');
+            bookList.innerHTML = "";
+            matchingBooks.forEach((book) =>{
+                const bookElement = book.render();
+            bookList.appendChild(bookElement);
+        })
+    }
     
 }
+
+//code used to test and console out functions to ensure code was working
+const testBook = new Book(["Bojoh, Adam"], 'en', ['Fantasy', 'Fiction'], 'A Amazing Storys of my Youth')
+console.log(testBook)
--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 // this is where the code that will be ran to activate contents from other js pages
 
 const theShelf = new Bookshelf()
-theShelf.addStack(bookData)
 // theShelf.sortAtoZ()
+
+theShelf.addBook(testBook)
+theShelf.addStack(bookData)
+
+
 theShelf.shelfStocker()
 
-// const userInput = document.querySelector('.userInput');
-// const sortButton = document.querySelector('.sorting');
+const userInput = document.querySelector('.userInput');
+const sortButton = document.querySelector('.sorting');
 
-// sortButton.addEventListener('click', function() {
-//     if (userInput.value) {
-//         theShelf.search(userInput.value);
-//     } else {
-//         theShelf.shelfStocker();
-//     }
-// })
+sortButton.addEventListener('click', function() {
+    if (userInput.value) {
+        theShelf.search(userInput.value);
+    } else {
+        theShelf.shelfStocker();
+    }
+})
